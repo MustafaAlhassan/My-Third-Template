81d32bedd4a2347b1475914203941c4e25074e10
Mustafa Alhassan <mustafamusab266@gmail.com>
2025-07-02 12:00:59 +0300

    add JavaScript to the template, with add some movement on the our-skills, events, and stats sections

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// To Increase the bars of our skills section
+let ourSkills = document.querySelector(".our-skills");
+let progressSpan = document.querySelectorAll(".progress span");
+
+window.onscroll = function () {
+  if (window.scrollY > ourSkills.offsetTop + 50) {
+    progressSpan.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
+
+// To Count the date down of Events Section
+let eventDate = new Date("Jul 1, 2030 23:59:59").getTime();
+let countDown = setInterval(() => {
+  let currentDate = new Date().getTime();
+  let dateDiff = eventDate - currentDate;
+
+  document.querySelector(".events .unit .days").innerHTML = Math.floor(
+    dateDiff / (1000 * 60 * 60 * 24)
+  );
+  document.querySelector(".events .unit .hours").innerHTML = Math.floor(
+    (dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  document.querySelector(".events .unit .minutes").innerHTML = Math.floor(
+    (dateDiff % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  document.querySelector(".events .unit .seconds").innerHTML = Math.floor(
+    (dateDiff % (1000 * 60)) / 1000
+  );
+}, 1000);
+
+// To Increase the number of stats section
+let stats = document.querySelector(".stats");
+let numbers = document.querySelectorAll(".stats .box .number");
+let started = false;
+
+function increaseNum(number) {
+  let goal = number.dataset.goal;
+  let count = setInterval(() => {
+    number.textContent++;
+    if (number.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+window.onscroll = function () {
+  if (window.scrollY > stats.offsetTop - 150) {
+    if (!started) {
+      numbers.forEach((number) => increaseNum(number));
+    }
+    started = true;
+  }
+};
